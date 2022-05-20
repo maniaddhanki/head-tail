99905abe85dfcb666ae1789c286a06ce62e24d1b
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-05-20 20:11:34 +0530

    renamed head as headLib

--- head.js
+++ head.js
@@ -1,1 +1,8 @@
+const fs = require('fs');
+const { headMain } = require('./src/headLib.js');
+
+const main = () => headMain(fs.readFileSync, args);
+
+const args = process.argv.slice(2);
+
 console.log('usage: head [-n lines | -c bytes] [file ...]');
