afd0609852d562c1382a17c6601fc39fc6b8b5d1
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-05-20 13:12:54 +0530

    set default count to 2
    * head working for content more than default count

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 **TODO :**
 - [ ] make `head.js` src directory
-- [ ] make head to give first ten lines of given content
+- [ ] set default count to 10
 
 **MAYBE :**
 
@@ -8,6 +8,7 @@
 - [x] make `src` and `test` directories
 - [x] make `testHead.js`
 - [x] write a test case for head
-- [x] make head work for 1 line content
 - [x] make head work on provided content instead of file
-- [x] make head work for content less than 10 lines
+- [x] make head work for 1 line content
+- [x] make head work for content less than default count
+- [x] make default count 2 
--- test/testHead.js
+++ test/testHead.js
@@ -1,13 +1,22 @@
 const assert = require('assert');
 
-const head = content => content;
+const head = content => {
+  const lines = content.split('\n');
+  if (lines.length <= 2) {
+    return lines.join('\n');
+  }
+  return lines.slice(0, 2).join('\n') + '\n';
+};
 
 describe('head', () => {
   it('should give content back when it is of one line', () => {
     assert.strictEqual(head('hello'), 'hello');
     assert.strictEqual(head('hi'), 'hi');
   });
-  it('should give content back when it is less than 10 lines', () => {
+  it('should give content back when lines are less than default count', () => {
     assert.strictEqual(head('hello\nbye'), 'hello\nbye');
+  });
+  it('should give first two lines of content', () => {
+    assert.strictEqual(head('a\nb\nc'), 'a\nb\n');
   });
 });
