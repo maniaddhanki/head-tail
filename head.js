cf953dd06ffab4aad25ac4ca041c9ca5958d7e76
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-05-21 16:09:13 +0530

    Remove usage in head.js

--- head.js
+++ head.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
 const { headMain } = require('./src/headLib.js');
 
-const main = () => console.log(headMain(fs.readFileSync, args));
+const main = () => {
+  try {
+    console.log(headMain(fs.readFileSync, args));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 const args = process.argv.slice(2);
 
-console.log('usage: head [-n lines | -c bytes] [file ...]');
 main();
