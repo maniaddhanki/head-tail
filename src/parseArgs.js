b3a999f54dbb3b7a904b2d4f91f9bb97a92f7f54
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-05-21 12:43:42 +0530

    Refactored parseArgs
    * Extracted parseWithOption and defaultOption functions

--- src/parseArgs.js
+++ src/parseArgs.js
@@ -1,13 +1,20 @@
-const parseArgs = function (args) {
+const defaultOption = function (args) {
+  const files = args;
+  const option = { count: 10 };
+  return { files, option };
+};
+
+const parseWithOption = function (args) {
   const keys = { '-n': 'count', '-c': 'byte' };
-  const options = {};
-  let file = args;
-  if (args[0].includes('-')) {
-    const option = keys[args[0]];
-    options[option] = +args[1];
-    file = args.slice(2);
-  }
-  return { file, options };
+  const option = {};
+  const key = keys[args[0]];
+  option[key] = +args[1];
+  const files = args.slice(2);
+  return { files, option };
+};
+
+const parseArgs = function (args) {
+  return (/^-/).test(...args) ? parseWithOption(args) : defaultOption(args);
 };
 
 exports.parseArgs = parseArgs;
